2fa4977437f741f8d353b33964a2abe426ec9587
Martin <gagochorenmartin@gmail.com>
2022-11-05 21:05:17 +0100

    json y carga en pantalla

--- script/app.js
+++ script/app.js
@@ -1,30 +1,58 @@
 "use strict";
 const listContacto = document.querySelector(".agenda-personas");
-let contacto = [];
+
+const fotoPerfil = document.querySelector(".foto-perfil");
+const telefono = document.querySelector(".telefono");
+const nombre = document.querySelector(".nombre");
+const puesto = document.querySelector(".puesto");
+const web = document.querySelector(".web");
+const mail = document.querySelector(".mail");
+
+
+const contactos = new Map();
 
 async function importarContactos(){
-    const response = await fetch('/script/contactos.json');
-    contacto = await response.json();
-    
+    // Traemos los contactos desde la BBDD y los metemos en un MAP
+    const response = await fetch('script/contactos.json');
+    let contacto = await response.json();
     contacto.forEach(element => {
-        const persona = document.createElement('li');
-        persona.className = "contacto"
-        persona.id = `${element.ID}`
-        persona.innerHTML += `
+        contactos.set(element.id, element);
+        // Creamos contenido para cada contacto
+        const elementoContacto = document.createElement('li');
+        elementoContacto.className = "contacto";
+        elementoContacto.id = element.id;
+        elementoContacto.innerHTML += `
         <div>
-        <img src="${element.imagen}" alt="imagen persona contacto">
-        <p>${element.nombre + " "+  element.apellido}</p>
-        <div>
+            <img src="${element.imagen}" alt="test">
+            <p>${element.nombre + " " + element.apellido} </p>
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
         `
-        listContacto.appendChild(persona);
+        listContacto.appendChild(elementoContacto)
 
-        persona.addEventListener("click",
-        
+        // Se pinta por pantalla los datos de los contactos
+        elementoContacto.addEventListener("click",
         ()=> {
-            console.log(persona)
+            fotoPerfil.src = `${element.imagen}`
+            nombre.textContent = `${element.nombre}`;
+            puesto.textContent = `${element.puesto}`;
+            telefono.textContent = `${element.telefono}`;
+            web.textContent = `${element.url}`;
+                web.href = `${element.url}`;
+            mail.textContent = `${element.email}`;
+                mail.href = `mailto:${element.email}`
         })
     }
-    
     );   
+    fotoPerfil.src = `${contactos.get("000").imagen}`
+    nombre.textContent = `${contactos.get("000").nombre}`; 
+    puesto.textContent = `${contactos.get("000").puesto}`;
+    telefono.textContent = `${contactos.get("000").telefono}`;
+    web.textContent = `${contactos.get("000").url}`; 
+        web.href= `${contactos.get("000").url}`
+    mail.textContent = `${contactos.get("000").email}`;
+        mail.href = `mailto:${contactos.get("000".email)}`
+
+
 }
 importarContactos();
