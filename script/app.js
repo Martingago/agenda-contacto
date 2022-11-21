7463abf354c78b3dbc402ddf96b4372d5b65fabc
Martin <gagochorenmartin@gmail.com>
2022-11-21 10:29:43 +0100

    más contactos en el JSON, diseño para movil, y ajustes JS

--- script/app.js
+++ script/app.js
@@ -1,6 +1,8 @@
 "use strict";
 const listContacto = document.querySelector(".agenda-personas");
 
+
+const PerfilFicha = document.querySelector(".profile-ppl");
 const fotoPerfil = document.querySelector(".foto-perfil");
 const telefono = document.querySelector(".telefono");
 const nombre = document.querySelector(".nombre");
@@ -21,6 +23,7 @@ async function importarContactos(){
         const elementoContacto = document.createElement('li');
         elementoContacto.className = "contacto";
         elementoContacto.id = element.id;
+        //Introducimos HTML un elemento DIV para cada contacto de nuestra lista
         elementoContacto.innerHTML += `
         <div>
             <img src="${element.imagen}" alt="test">
@@ -30,9 +33,10 @@ async function importarContactos(){
         `
         listContacto.appendChild(elementoContacto)
 
-        // Se pinta por pantalla los datos de los contactos
+        // Se pinta por pantalla los datos de los contactos cada vez que el usuario hace click en un componente
         elementoContacto.addEventListener("click",
         ()=> {
+            PerfilFicha.style.display = "flex";
             fotoPerfil.src = `${element.imagen}`
             nombre.textContent = `${element.nombre}`;
             puesto.textContent = `${element.puesto}`;
@@ -44,6 +48,8 @@ async function importarContactos(){
         })
     }
     );   
+    //Inicializamos la pagina con el contacto número 0.
+    // Wow esto é moi cutre NECESITA MEJORAAAAAAAAAAAAA
     fotoPerfil.src = `${contactos.get("000").imagen}`
     nombre.textContent = `${contactos.get("000").nombre}`; 
     puesto.textContent = `${contactos.get("000").puesto}`;
@@ -56,3 +62,33 @@ async function importarContactos(){
 
 }
 importarContactos();
+
+
+// Botón para mostrar ocultar contactos resoluciones pequeñas
+
+const btnCerrar = document.querySelector(".btn-close");
+
+
+btnCerrar.addEventListener(
+    "click",
+    ()=>{
+        PerfilFicha.style.display = "none";
+    }
+);
+
+//Ajsute para que entre cambio de resoluciones el contenido se ponga visible o no
+//En movil por defecto no se verá la ficha del contacto hasta que hagas click
+//En altas resoluciones siempre se mostrará la tabla del último usuario clickado,
+//o e su defecto el primero de la lista.
+window.addEventListener(
+    "resize",
+    ()=> {
+        if(innerWidth > 700){
+          PerfilFicha.style.display = "flex"  
+        }else{
+            PerfilFicha.style.display = "none" 
+        }
+
+    }
+);
+
